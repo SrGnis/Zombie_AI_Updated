79ab79f29a3d89780a301bab2f5cf58010d37d25
SrGnis <gnisms97@gmail.com>
2021-01-31 23:45:37 +0100

    Initial commit

---
+++ src/main/java/com/srgnis/zombieaiupdated/ZombieAIUpdated.java
@@ -0,0 +1,10 @@
+package com.srgnis.zombieaiupdated;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ZombieAIUpdated implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}

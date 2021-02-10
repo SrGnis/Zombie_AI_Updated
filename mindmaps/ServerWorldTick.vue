1ec3a0a80d1c47007232153dde2870440955be06
SrGnis <gnisms97@gmail.com>
2021-02-10 22:57:17 +0100

    ServerWorldTick started

---
+++ mindmaps/ServerWorldTick.vue
@@ -0,0 +1,82 @@
+<!-- Tufts VUE 3.3.0 concept-map (ServerWorldTick.vue) 2021-02-10 -->
+<!-- Tufts VUE: http://vue.tufts.edu/ -->
+<!-- Do Not Remove: VUE mapping @version(1.1) jar:file:/C:/Program%20Files%20(x86)/VUE/VUE.jar!/tufts/vue/resources/lw_mapping_1_1.xml -->
+<!-- Do Not Remove: Saved date Wed Feb 10 22:55:56 CET 2021 by Genis on platform Windows 8 6.2 in JVM 1.7.0_21-b11 -->
+<!-- Do Not Remove: Saving version @(#)VUE: built October 8 2015 at 1658 by tomadm on Linux 2.6.32-504.23.4.el6.x86_64 i386 JVM 1.7.0_21-b11(bits=32) -->
+<?xml version="1.0" encoding="US-ASCII"?>
+<LW-MAP xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:noNamespaceSchemaLocation="none" ID="0"
+    label="ServerWorldTick.vue" created="1612994109861" x="0.0" y="0.0"
+    width="1.4E-45" height="1.4E-45" strokeWidth="0.0" autoSized="false">
+    <resource referenceCreated="1612994157088"
+        spec="H:\Fabric Projects\Zombie_AI_Updated\mindmaps\ServerWorldTick.vue"
+        type="1" xsi:type="URLResource">
+        <title>ServerWorldTick.vue</title>
+        <property key="File" value="H:\Fabric Projects\Zombie_AI_Updated\mindmaps\ServerWorldTick.vue"/>
+    </resource>
+    <fillColor>#FFFFFF</fillColor>
+    <strokeColor>#404040</strokeColor>
+    <textColor>#000000</textColor>
+    <font>SansSerif-plain-14</font>
+    <URIString>http://vue.tufts.edu/rdf/resource/8df19a26c0a801140175f8cb1980fb2e</URIString>
+    <layer ID="1" label="Layer 1" created="1612994109861" x="0.0"
+        y="0.0" width="1.4E-45" height="1.4E-45" strokeWidth="0.0" autoSized="false">
+        <URIString>http://vue.tufts.edu/rdf/resource/8df19a26c0a801140175f8cb927796e2</URIString>
+    </layer>
+    <userZoom>1.0</userZoom>
+    <userOrigin x="-14.0" y="-14.0"/>
+    <presentationBackground>#202020</presentationBackground>
+    <PathwayList currentPathway="0" revealerIndex="-1">
+        <pathway ID="0" label="Camino sin t&#xed;tulo"
+            created="1612994109860" x="0.0" y="0.0" width="1.4E-45"
+            height="1.4E-45" strokeWidth="0.0" autoSized="false"
+            currentIndex="-1" open="true">
+            <strokeColor>#B3CC33CC</strokeColor>
+            <textColor>#000000</textColor>
+            <font>SansSerif-plain-14</font>
+            <URIString>http://vue.tufts.edu/rdf/resource/8df19a28c0a801140175f8cb6a7af3cd</URIString>
+            <masterSlide ID="2" created="1612994109889" x="0.0" y="0.0"
+                width="800.0" height="600.0" locked="true"
+                strokeWidth="0.0" autoSized="false">
+                <fillColor>#000000</fillColor>
+                <strokeColor>#404040</strokeColor>
+                <textColor>#000000</textColor>
+                <font>SansSerif-plain-14</font>
+                <URIString>http://vue.tufts.edu/rdf/resource/8df19a28c0a801140175f8cb4b361e4c</URIString>
+                <titleStyle ID="3" label="Header"
+                    created="1612994109907" x="335.5" y="172.5"
+                    width="129.0" height="55.0" strokeWidth="0.0"
+                    autoSized="true" isStyle="true" xsi:type="node">
+                    <strokeColor>#404040</strokeColor>
+                    <textColor>#FFFFFF</textColor>
+                    <font>Gill Sans-plain-36</font>
+                    <URIString>http://vue.tufts.edu/rdf/resource/8df19a29c0a801140175f8cb4009fe9d</URIString>
+                    <shape xsi:type="rectangle"/>
+                </titleStyle>
+                <textStyle ID="4" label="Slide Text"
+                    created="1612994109908" x="346.5" y="281.5"
+                    width="107.0" height="37.0" strokeWidth="0.0"
+                    autoSized="true" isStyle="true" xsi:type="node">
+                    <strokeColor>#404040</strokeColor>
+                    <textColor>#FFFFFF</textColor>
+                    <font>Gill Sans-plain-22</font>
+                    <URIString>http://vue.tufts.edu/rdf/resource/8df19a2ac0a801140175f8cb19ff520e</URIString>
+                    <shape xsi:type="rectangle"/>
+                </textStyle>
+                <linkStyle ID="5" label="Links" created="1612994109909"
+                    x="373.5" y="384.0" width="53.0" height="32.0"
+                    strokeWidth="0.0" autoSized="true" isStyle="true" xsi:type="node">
+                    <strokeColor>#404040</strokeColor>
+                    <textColor>#B3BFE3</textColor>
+                    <font>Gill Sans-plain-18</font>
+                    <URIString>http://vue.tufts.edu/rdf/resource/8df19a2ac0a801140175f8cb1a08e2f9</URIString>
+                    <shape xsi:type="rectangle"/>
+                </linkStyle>
+            </masterSlide>
+        </pathway>
+    </PathwayList>
+    <date>2021-02-10</date>
+    <modelVersion>6</modelVersion>
+    <saveLocation>H:\Fabric Projects\Zombie_AI_Updated\mindmaps</saveLocation>
+    <saveFile>H:\Fabric Projects\Zombie_AI_Updated\mindmaps\ServerWorldTick.vue</saveFile>
+</LW-MAP>
